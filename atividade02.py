1cc356ae8bd5039b5cf9c8713b39750dd4c7da3c
vitorlopes1248 <vitorlopes1248@gmail.com>
2024-09-04 14:54:04 -0300

    atividade02 celsius para fahrenheit

---
+++ atividade02.py
@@ -0,0 +1,4 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+celsius = int(input("digite a temperatura em celsius: "))
+transformar = celsius * 9/5 + 32
+print("sua temperatura em fahrenheit: ", transformar)
